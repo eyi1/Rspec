7e2ac3af2496d59fdf6facd710f36b70ca9575e1
github username <ej7729@gmail.com>
2019-06-17 11:37:50 -0400

    add sessions and login routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,14 @@
 Rails.application.routes.draw do
-  resources :posts
-  resources :users
+  # resources :posts
+  # resources :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  namespace :api do 
+    namespace :v1 do 
+      post '/signup', to: "users#signup"
+      post '/login', to: "sessions#login"
+      get '/posts', to: "posts#index"
+    end
+  end
+
 end
