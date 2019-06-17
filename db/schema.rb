88ab68a89b133d2dc1809adcebda9518632fcfad
github username <ej7729@gmail.com>
2019-06-17 11:36:28 -0400

    update database

--- db/schema.rb
+++ db/schema.rb
@@ -22,6 +22,8 @@ ActiveRecord::Schema.define(version: 2019_06_01_215735) do
 
   create_table "users", force: :cascade do |t|
     t.string "name"
+    t.string "email"
+    t.string "password_digest"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
