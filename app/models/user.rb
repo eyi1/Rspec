32ff0ca616e74dcd3bcc0d407146f2ba3a715295
github username <ej7729@gmail.com>
2019-06-17 11:38:35 -0400

    signup action invokes jwt create method

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,9 @@
 class User < ApplicationRecord
+    has_secure_password
+    
     has_many :posts
 
-    validates_presence_of :name
+    #validates_presence_of :name, :email, :password
+    validates :name, :email, presence: true
+    validates :name, :email, uniqueness: true
 end
