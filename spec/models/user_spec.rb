26f0ddfb4a4f52181b066749344eecb37de6ea0c
github username <ej7729@gmail.com>
2019-06-24 22:51:38 -0400

    delete unused/commented code

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,4 +1,4 @@
-# require 'rails_helper'
+require 'rails_helper'
 
 # RSpec.describe User, type: :model do
 #   before(:each) do
@@ -10,37 +10,6 @@
 # #     expect(@user.posts.count).to eq(1)
 # #   end
 # end
-
-require 'rails_helper'
-
-# RSpec.describe User, type: :model do
-#   subject { described_class.new(
-#       name: 'Anything') 
-#   }   
-  
-#   it "is not valid without a name" do
-#         subject.name = nil
-#         expect(subject).to be_valid
-#     end
-    
-#    it "is valid with valid attributes" do
-#         expect(subject).to be_valid
-#     end
-#   end
-
-
-# RSpec.describe User, :type => :model do
-#     it "is valid with valid attributes" do
-#         expect(User.new(name: 'anything')).to be_valid
-#     end
-
-#     # it "is not valid without a name" do
-#     #     user = User.new(name: nil)
-#     #     expect(user).to_not be_valid
-#     # end
-#     # it "is not valid without a start_date"
-#     # it "is not valid without a end_date"
-#   end
 
 RSpec.describe User, :type => :model do
     subject { described_class.new(name: "Anything") }
@@ -63,4 +32,3 @@ end
 # RSpec.describe User, type: :model do
 #     it { should validate_presence_of :name }
 #   end
-
